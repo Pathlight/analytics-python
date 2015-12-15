d6581eceb70490ba3d7c23d8ac6ca419272fb6d6
doigoid <trey@pathlight.co>
2015-12-15 10:54:36 -0800

    Adding datetime,date support to the json serializer

--- analytics/request.py
+++ analytics/request.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.tz import tzutc
 import logging
 import json
@@ -49,5 +49,7 @@ class DatetimeSerializer(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
             return obj.isoformat()
+        if isinstance(obj, date):
+            return obj.strftime("%Y-%m-%d")
 
         return json.JSONEncoder.default(self, obj)
